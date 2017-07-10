3ad4fe300b5f5ecfa86f62b91456c5f8c25279a6
Johannes Probst <johannes.probst@authenticvision.com>
2017-07-10 18:05:53 +0200

    adds new stocks resource for demo-data-server; adapts demo-data-server for dynamic file usage; changes dynamic-data-server output of multiple data objects to a valid root json object; adds a valid line-chart; TODO: 1) still problems with the new line-chart and lifo mode; 2) an array of multiple data objects from demo-data-server causes exception in DataTopic's processing.

--- demo/src/main/java/App.java
+++ demo/src/main/java/App.java
@@ -3,38 +3,75 @@ import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.List;
 
 public class App {
+
+    static Iterator<String> dataIterator;
+
+    static List fileContentList;
+
+    static String path;
 
     public static void main(String... args ) throws Exception{
         MockWebServer server = new MockWebServer();
 
-
-        Iterator<String> sensorDataIterator = Files.readAllLines(Paths.get(ClassLoader.getSystemResource("sensordata1.json").toURI())).iterator();
         Dispatcher dispatcher = new Dispatcher() {
             @Override
             public MockResponse dispatch(RecordedRequest request) throws InterruptedException {
+
                 MockResponse response = new MockResponse();
-                response.addHeader("Content-Type","application/json");
-                StringBuilder builder = new StringBuilder(1024);
-                builder.append("[");
-                String queryParam = request.getRequestUrl().queryParameter("n");
-                Integer n = queryParam == null? 1 : Integer.parseInt(queryParam);
-                do{
-                    builder.append(sensorDataIterator.next());
-                    if(n>1)
-                        builder.append(",");
-                    n-=1;
-                }while (n>0);
-                builder.append("]");
-                response.setBody(builder.toString());
+                if(!request.getRequestUrl().pathSegments().get(0).equals("favicon.ico")){
+
+                    response.addHeader("Content-Type","application/json");
+                    StringBuilder builder = new StringBuilder(1024);
+                    String queryParam = request.getRequestUrl().queryParameter("n");
+
+                    try {
+                        if(App.dataIterator == null || !request.getRequestUrl().pathSegments().get(0).equals(App.path) )
+                            App.dataIterator = getResourceData(request.getRequestUrl().pathSegments().get(0));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        return response.setBody(e.toString());
+                    }
+                    Boolean isRequestForMultipleValues = false;
+                    Integer n = queryParam == null? 1 : Integer.parseInt(queryParam);
+                    if(n>1) {
+                        isRequestForMultipleValues = true;
+                        builder.append("{ \"values\": ");
+                    }
+                        builder.append("[ ");
+                    do{
+                        if (!App.dataIterator.hasNext()){
+                            App.dataIterator = App.fileContentList.iterator();
+                        }
+                        builder.append(App.dataIterator.next());
+                        if(n>1)
+                            builder.append(",");
+                        n-=1;
+                    }while (n>0);
+
+                    builder.append("]");
+                    if(isRequestForMultipleValues){
+                        builder.append(" }");
+                    }
+                    response.setBody(builder.toString());
+                }
                 return response;
             }
         };
         server.setDispatcher(dispatcher);
         server.start(11369);
+    }
+
+    private static Iterator<String> getResourceData(String path) throws Exception {
+        App.path = path;
+        App.fileContentList = Files.readAllLines(Paths.get(ClassLoader.getSystemResource(path+".json").toURI()));
+        App.dataIterator = App.fileContentList.iterator();
+        return App.dataIterator;
     }
 }
